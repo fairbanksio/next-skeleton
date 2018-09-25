ad7329db61e38cac2cd392c7c60d943629988d27
Jon Fairbanks <fairplay89@gmail.com>
2018-09-24 19:32:35 -0700

    Convert Homepage to Material UI

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,129 @@
 import Link from 'next/link'
-import Buttons from '../components/buttons';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-export default () => (
-  <div>
-    <i className="fab fa-2x fa-react"></i>
-    <h2>Server-Side Rendered Routes</h2>
-    <ul>
-      <li><Link href='/page?id=first' as='/page/first'><a>My first page</a></Link></li>
-      <li><Link href='/page?id=second' as='/page/second'><a>My second page</a></Link></li>
-      <li><Link href='/page?id=third' as='/page/third'><a>My third page</a></Link></li>
-    </ul>
-    <Buttons/>
-  </div>
-)
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2,
+  },
+  heroUnit: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  heroButtons: {
+    marginTop: theme.spacing.unit * 4,
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit,
+    position: 'absolute',
+    width: '100vw',
+    bottom: '0px',
+  },
+});
+
+function Landing(props) {
+  const { classes } = props;
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroUnit}>
+          <div className={classes.heroContent}>
+            <Typography variant="display3" align="center" color="textPrimary" gutterBottom>
+              Next-Skeleton
+            </Typography>
+            <Typography variant="title" align="center" color="textSecondary" paragraph>
+              Something short and leading about the collection below—its contents, the creator, etc.
+              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+              entirely.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={16} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Get Started
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    More Info
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </div>
+        <div className={classNames(classes.layout, classes.cardGrid)}>
+          {/* End hero unit */}
+          <Grid container spacing={40}>
+            <div>
+              <i className="fab fa-2x fa-react"></i>
+              <br/>
+              <h2>Server-Side Rendered Routes:</h2>
+              <ul>
+                <li><Link href='/page?id=first' as='/page/first'><a>My first page</a></Link></li>
+                <li><Link href='/page?id=second' as='/page/second'><a>My second page</a></Link></li>
+                <li><Link href='/page?id=third' as='/page/third'><a>My third page</a></Link></li>
+              </ul>
+            </div>
+          </Grid>
+        </div>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="title" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subheading" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
+
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Landing);
