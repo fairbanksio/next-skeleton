dbf13f27aaf8c5a8d1b752ce83caa4555d9790f1
root <brandon.sorgdrager@gmail.com>
2018-09-25 00:29:42 +0000

    Implement proper SSR for material ui styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import App, {Container} from 'next/app'
 import NavBar from '../components/navbar';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getPageContext from '../components/getPageContext';
 
 class Layout extends React.Component {
   render () {
@@ -13,12 +17,46 @@ class Layout extends React.Component {
 }
 
 export default class MyApp extends App {
-  render () {
-    const {Component, pageProps} = this.props
-    return <Container>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Container>
+  constructor(props) {
+    super(props);
+    this.pageContext = getPageContext();
+  }
+
+  pageContext = null;
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        {/* Wrap every page in Jss and Theme providers */}
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server side. */}
+            <Layout>
+              <Component pageContext={this.pageContext} {...pageProps} />
+            </Layout>
+          </MuiThemeProvider>
+        </JssProvider>
+      </Container>
+    );
   }
 }
