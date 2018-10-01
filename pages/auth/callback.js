2cf13a07d46b5c13eaf26258f88328ffd554824f
Jon Fairbanks <fairplay89@gmail.com>
2018-09-30 21:22:55 -0700

    Redirect to /dashboard on Login

--- pages/auth/callback.js
+++ pages/auth/callback.js
@@ -12,19 +12,19 @@ export default class extends React.Component {
     const session = await NextAuth.init({force: true, req: req})
 
     const cookies = new Cookies((req && req.headers.cookie) ? req.headers.cookie : null)
-    
-    // If the user is signed in, we look for a redirect URL cookie and send 
+
+    // If the user is signed in, we look for a redirect URL cookie and send
     // them to that page, so that people signing in end up back on the page they
-    // were on before signing in. Defaults to '/'.
-    let redirectTo = '/'
+    // were on before signing in. Defaults to '/dashboard'.
+    let redirectTo = '/dashboard'
     if (session.user) {
       // Read redirect URL to redirect to from cookies
       redirectTo = cookies.get('redirect_url') || redirectTo
-      
+
       // Allow relative paths only - strip protocol/host/port if they exist.
       redirectTo = redirectTo.replace( /^[a-zA-Z]{3,5}\:\/{2}[a-zA-Z0-9_.:-]+\//, '')
     }
-    
+
     return {
       session: session,
       redirectTo: redirectTo
